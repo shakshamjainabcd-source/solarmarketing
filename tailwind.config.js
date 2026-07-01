cded6979ebd0a41c450fd08249bf4d331927a5ad
shakshamjainabcd-source <shakshamjainabcd@gmail.com>
2026-07-01 12:27:34 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        matteBlack: "#111111",
+        luxuryGold: "#D4AF37",
+        bloodRed: "#8B0000",
+        darkCard: "#1a1a1a",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
